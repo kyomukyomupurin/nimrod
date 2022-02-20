f3169c5ea1def81c140afbe6b127118844529570
kyomukyomupurin <murakami1728@gmail.com>
2022-02-20 23:03:20 +0900

    Initial commit

---
+++ nimrod/misc/template.cc
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bit>
+#include <cassert>
+#include <charconv>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <deque>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+using i64 = int64_t;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+
+
+  return 0;
+}
